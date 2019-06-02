057ae2375b7ed72ed86a4b9813f6dc56a9341fda
magechaP <magechapeter@gmail.com>
2019-06-02 18:12:59 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovingComponent } from './moving/moving/moving.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteInfoComponent } from './quote-info/quote-info.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovingComponent,
+    QuoteFormComponent,
+    QuoteInfoComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
